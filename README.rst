9a3932db957a7c60f71ede16bb2f49b6076d00da
Tran Xuan Tu <xtutran@gmail.com>
2018-07-17 21:21:04 +0700

    initialize the project

---
+++ README.rst
@@ -0,0 +1,8 @@
+LabguruPython Repository
+========================
+
+This project is a Python wrapper of Labguru API
+
+`Learn more <https://my.labguru.com/api/docs/>`_.
+
+---------------
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='labguru',
+    version='0.1.0',
+    description='Labguru Python API',
+    long_description=readme,
+    author='Tran Xuan Tu',
+    author_email='xtutran@gmail.com',
+    url='https://github.com/BioData/LabguruPython',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
